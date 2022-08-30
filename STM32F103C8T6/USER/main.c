d451326e36726b55d2669d7035489657aa20f5d0
PuzzyGong <763081477@qq.com>
2022-08-30 22:02:33 +0800

    stm32 add movement of y direction

--- STM32F103C8T6/USER/main.c
+++ STM32F103C8T6/USER/main.c
@@ -1,23 +1,33 @@
 //***************************************************//
+//接口：深度传感器：PA3
+//      树莓派的RX：PA9（STM32的TX）
+//      树莓派的TX：PA10（STM32的RX）
+//		控制深度螺旋桨：PA6 PA7
+//		控制水平螺旋桨：PB6 PB7 PB8 PB9（具体连接情况，在连接硬件的时候配合软件调）
+//
+//***************************************************//
 //STM32F103实现功能 1.深度控制器（PID需要自调）
-//                  2.前后速度控制器
+//                  2.xy速度控制器
 //                  2.旋转速度控制器
 //
 //串口数据输入格式（字符串）如下：
-//P_depth	I_depth		D_depth		a_depth 	v_x 	v_p 	pwm_max
+//P_depth	I_depth		D_depth		a_depth 	v_x 	v_y		v_p 	pwm_max
 //
 //其中：当v_x的值为0~100，船体向后行驶，
 //		         100~200，船体向前行驶。
+//其中：当v_y的值为0~100，船体向右行驶，
+//		         100~200，船体向左行驶。
 //其中：当v_p的值为0~100，船体向逆时针旋转，
 //		         100~200，船体向顺时针旋转。
 //其中：pwm_max = 100时，螺旋桨驱动力可以达到最强，即100%。
 //		pwm_max = x时，螺旋桨驱动力可以达到x%。
+//
 //***************************************************//
 
 #include "include.h"
 
 PID depth_pid;
-int v_x = 0, v_p = 0, pwm_max = 0;
+int v_x = 0, v_y = 0, v_p = 0, pwm_max = 0;
 
 //************** Parameter of depth_pid *************//
 int P_depth = 0, I_depth = 0, D_depth = 0;
@@ -53,16 +63,17 @@ int main(void)
 		{
 			depth = depth1 + 0.01 * depth2;
 		}		
-		if(trans_others_R(1, &P_depth, &I_depth, &D_depth, &a_depth, &v_x, &v_p, &pwm_max, 0, 0, 0))
+		if(trans_others_R(1, &P_depth, &I_depth, &D_depth, &a_depth, &v_x, &v_y, &v_p, &pwm_max, 0, 0))
 		{
-			v_x -= 100, v_p -= 100;
+			v_x -= 100, v_y -= 100, v_p -= 100;
 			PID_init(&depth_pid, P_depth, I_depth, D_depth, 0);
 			printf("P_depth = %d\r\n", P_depth);
 			printf("I_depth = %d\r\n", I_depth);
 			printf("D_depth = %d\r\n", D_depth);
 			printf("a_depth = %d\r\n", a_depth);
 			printf("v_x = %d\r\n", v_x);
-			printf("v_y = %d\r\n", v_p);
+			printf("v_y = %d\r\n", v_y);
+			printf("v_p = %d\r\n", v_p);
 			printf("pwm_max = %d\r\n", pwm_max);
 			printf("depth = %.2f\r\n\r\n\r\n", depth);
 		}
@@ -71,9 +82,9 @@ int main(void)
 		TIM_SetCompare1(TIM3, pwm_limit(pwm_depth));
 		TIM_SetCompare2(TIM3, pwm_limit(pwm_depth));
 		
-		TIM_SetCompare1(TIM4, pwm_limit(v_x + v_p));
-		TIM_SetCompare2(TIM4, pwm_limit(v_x - v_p));
-		TIM_SetCompare3(TIM4, pwm_limit(v_x - v_p));
-		TIM_SetCompare4(TIM4, pwm_limit(v_x + v_p));
+		TIM_SetCompare1(TIM4, pwm_limit(v_x + v_y + v_p));
+		TIM_SetCompare2(TIM4, pwm_limit(v_x - v_y - v_p));
+		TIM_SetCompare3(TIM4, pwm_limit(v_x + v_y - v_p));
+		TIM_SetCompare4(TIM4, pwm_limit(v_x - v_y + v_p));
 	}
 }
